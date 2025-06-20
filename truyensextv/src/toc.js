4c6eecdda02b083a46238332c5a78dc4e25a3aae
639936 <hoahockibi@gmail.com>
2025-06-21 06:48:29 +0700

    up

--- truyensextv/src/toc.js
+++ truyensextv/src/toc.js
@@ -1,7 +1,6 @@
 load('config.js');
 
 function execute(url) {
-    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL)
     var response = fetch(url);
     if (response.ok) {
         var doc = response.html();
