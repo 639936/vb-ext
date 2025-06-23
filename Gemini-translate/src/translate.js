8e5d7f892541cf25bd0499e66526d6fe811ee88b
639936 <hoahockibi@gmail.com>
2025-06-24 02:38:55 +0700

    up

--- Gemini-translate/src/translate.js
+++ Gemini-translate/src/translate.js
@@ -1,7 +1,7 @@
 load("language_list.js"); 
 
 function execute(text, from, to, apiKey1) {
-    let apiKey = "";
+    let apiKey = "no"
     if (!apiKey) {
         return Response.success("");
     }
