56f4c1618926b970b21456fdacf897352cbd8aac
639936 <hoahockibi@gmail.com>
2025-06-25 01:53:54 +0700

    up

--- xtv2/src/home.js
+++ xtv2/src/home.js
@@ -7,5 +7,5 @@ function execute() {
         {title: "Truyện sex dài tập", input: BASE_URL + "/truyen-sex-dai-tap/", script: "up3.js"},
         {title: "Top 300 truyện ngắn được đọc nhiều nhất", input: BASE_URL + "/top-300-truyen-ngan-duoc-doc-nhieu-nhat", script: "up2.js"},
         {title: "Top 1000 truyện sex được đọc nhiều nhất", input: BASE_URL + "/top-1000-truyen-sex-duoc-doc-nhieu-nhat", script: "up2.js"},
-    ])
+    ]);
 }
