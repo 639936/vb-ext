8ca9d35b7935bb64d217de89a4b97b9e615c12f0
639936 <hoahockibi@gmail.com>
2025-06-24 20:44:51 +0700

    xtv2 update 1

--- xtv2/src/up2.js
+++ xtv2/src/up2.js
@@ -1,3 +1,24 @@
 load('config.js');
 
-function execute(url, page) {
+function execute(url) {
+    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
+    var response = fetch(url);
+    if (response.ok) {
+        var doc = response.html().select(".ndtruyen")
+        doc.select("> p").first().remove()
+        doc.select("> em ").last().remove();
+        var nd = doc.select(".list2")
+        var data = []
+        for (var i = 0; i < nd.size(); i++ ){
+            var el = nd.get(i)
+            data.push({
+                name: el.text(),
+                link: el.select("a").attr("href"),
+                cover: null,
+                description: null,
+                host:  BASE_URL
+            })
+        }
+        return Response.success(data) 
+    }
+}
--- xtv2/src/up3.js
+++ xtv2/src/up3.js
@@ -1,3 +1,26 @@
 load('config.js');
 
-function execute(url, page) {
+function execute(url) {
+    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
+    var response = fetch(url);
+    if (response.ok) {
+        var doc = response.html().select("body")
+        doc.select(".noibat").last().remove()
+        doc.select(".noibat ").last().remove();
+        var nd1 = doc.select(".noibat")
+        var nd2 = doc.select(".noibat + .bai-viet-box")
+        var data = []
+        for (var i = 2; i < nd2.size(); i++ ){
+            var e1 = nd1.get(i).select("a").first()
+            var e2 = nd2.get(i).select("a").first()
+            data.push({
+                name: e1.text(),
+                link: e1.attr("href"),
+                cover: null,
+                description: e2.text(),
+                host:  BASE_URL
+            })
+        }
+        return Response.success(data) 
+    }
+}
