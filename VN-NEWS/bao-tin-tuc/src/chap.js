0010fbbe47b857b5f85dc82f015b52798f7cf9aa
639936 <hoahockibi@gmail.com>
2025-07-03 11:35:04 +0700

    up

--- VN-NEWS/bao-tin-tuc/src/chap.js
+++ VN-NEWS/bao-tin-tuc/src/chap.js
@@ -1,5 +1,16 @@
 load('config.js');
 
 function execute(url) {
-return Response.success(BASE_URL + url)
+    let response = fetch(url);
+    if (response.ok) {
+        let doc = response.html().select(".contents");
+        doc.select(".lazier").remove();
+        doc.select("script").remove();
+        doc.select("noscript").remove();
+        doc.select("ins").remove();
+        let htm = doc.html()
+
+        return Response.success(htm);
+    }
+    return null;
 }
