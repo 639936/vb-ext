bf784c8f52312d95ca0409fa416307f24e2f3b4d
639936 <hoahockibi@gmail.com>
2025-07-04 11:12:15 +0700

    update

--- Local-book/src/chap.js
+++ Local-book/src/chap.js
@@ -3,7 +3,7 @@ function execute(url) {
     var response = fetch(url);
     if (response.ok) {
         let doc = response.html();
-        var content = doc.select(".contentbox").html();
+        var content = doc.select(".content").html();
         return Response.success(content);
     }
     return null;
