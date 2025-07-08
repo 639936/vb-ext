f7e3483b51fac6f04a5f9a6c6b7ce3f781ee3c72
639936 <hoahockibi@gmail.com>
2025-07-08 19:47:37 +0700

    up

--- Gemini-Search-Name/src/language.js
+++ Gemini-Search-Name/src/language.js
@@ -1,10 +1,5 @@
 load("language_list.js");
 
 function execute() {
-    // Trả về một đối tượng chứa cả hai danh sách
-    // Giả sử ứng dụng có thể xử lý việc này để điền vào hai menu khác nhau
-    return Response.success({
-        from: sourceLanguages,
-        to: targetModes
-    });
+    return Response.success(languages);
 }
