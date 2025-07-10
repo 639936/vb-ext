250240a625871dc1cc36ab9fab48815ca52a94f9
639936 <hoahockibi@gmail.com>
2025-07-11 02:11:07 +0700

    update

--- TruyenC/src/toc.js
+++ TruyenC/src/toc.js
@@ -4,8 +4,7 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok){
         let doc = fetch(url).html()
-        let el = doc.select('#tab-chapters a')
-
+        let el = doc.select('#tab-chapters a, #tab-latest-chapters a')
         el.forEach(e => {
             chapters.push({
                 name: e.select("span").text(),
