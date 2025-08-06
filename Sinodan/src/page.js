6d0701f975966266788ec71f12975ad78ca366d6
639936 <hoahockibi@gmail.com>
2025-08-06 18:07:28 +0700

    ok

--- Sinodan/src/page.js
+++ Sinodan/src/page.js
@@ -5,10 +5,14 @@ function execute(url) {
     if (response.ok) {
         let doc = response.html();
         var page = [];
+        let check = doc.select(".pagelistbox option");
+    if (check) page.push(url)
+    else {
         doc.select(".pagelistbox option").forEach(e => {
           page.push(BASE_URL + e.attr("value"))
         });
         return Response.success(page)
+    }
     }
     return null;
 }
