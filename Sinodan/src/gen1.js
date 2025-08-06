9e03e85a08c71f6a8f853e45fcc5463b99288d58
639936 <hoahockibi@gmail.com>
2025-08-06 17:35:02 +0700

    ok

--- Sinodan/src/gen1.js
+++ Sinodan/src/gen1.js
@@ -2,19 +2,18 @@ load('config.js');
 
 function execute(url, page) {
     if (!page) page = '1';
-    let response = fetch(url);
-        if (response.ok) {
-            let doc = response.html();
+    let response1 = fetch(url);
+        if (response1.ok) {
+            let doc = response1.html();
             let newUrl = doc.select("select option").first().attr("value");
             url = newUrl.match(/(.*_)/)[0];
-
+        }
     let fullUrl = BASE_URL + url + page + ".html"; 
     let response = fetch(fullUrl);
-
     if (response.ok) {
-        let doc = response.html();
+        let doc1 = response.html();
         var data = [];
-        doc.select(".mod.block.book-all-list ul li").forEach(e => {
+        doc1.select(".mod.block.book-all-list ul li").forEach(e => {
             data.push({
                 name: e.select("a.name").text(),
                 link: e.select("a.name").attr("href"),
