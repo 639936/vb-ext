b68b5f00148b9bac810a4dce4365c4ed48d53420
639936 <hoahockibi@gmail.com>
2025-08-19 05:04:54 +0700

    uo

--- bing-tts/src/tts.js
+++ bing-tts/src/tts.js
@@ -38,7 +38,7 @@ function findBingData() {
 
 function generateSSML(text, voiceLang, voiceName, voiceGender) {
     // Thêm volume='loud' vào thẻ prosody
-    return "<speak version='1.0' xml:lang='" + voiceLang + "'><voice xml:lang='" + voiceLang + "' xml:gender='" + voiceGender + "' name='" + voiceName + "'><prosody rate='0%' volume='x-loud'>" + escapeXml(text) + "</prosody></voice></speak>"
+    return "<speak version='1.0' xml:lang='" + voiceLang + "'><voice xml:lang='" + voiceLang + "' xml:gender='" + voiceGender + "' name='" + voiceName + "'><prosody rate='0%' volume='loud'>" + escapeXml(text) + "</prosody></voice></speak>"
 }
 
 function escapeXml(unsafe) {
