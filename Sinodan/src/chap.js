3808ea2216a13f9403594e1f855c84c39c1903b3
639936 <hoahockibi@gmail.com>
2025-08-19 14:41:40 +0700

    uo

--- Sinodan/src/chap.js
+++ Sinodan/src/chap.js
@@ -2,7 +2,7 @@ load('config.js');
 function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
 
-    let response = fetch( BASE_URL + url);
+    let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
         doc.select("center").remove();
