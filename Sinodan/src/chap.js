edde320128415edd9c62c28ba78749d88bc74de4
639936 <hoahockibi@gmail.com>
2025-08-19 14:39:45 +0700

    up

--- Sinodan/src/chap.js
+++ Sinodan/src/chap.js
@@ -7,7 +7,18 @@ function execute(url) {
         let doc = response.html();
         doc.select("center").remove();
         let htm = doc.select("#nr1").html();
+        htm = cleanHtml(htm);
         return Response.success(htm);
     }
     return null;
+}
+function cleanHtml(htm) {
+    htm = htm.replace(/(<br>\s*){2,}/g, '<br>');
+    htm = htm.replace(/<br>/g, '\n');
+    htm = htm.replace(/\n\n\n/g, '\n\n');
+    htm = htm.replace(/<a[^>]*>([^<]+)<\/a>/g, '');
+    htm = htm.replace(/&(nbsp|amp|quot|lt|gt);/g, "");
+    htm = htm.replace(/<!--(<br \/>)?[^>]*-->/gm, '');
+    htm = htm.replace(/\&nbsp;/g, "");
+    return htm;
 }
