0a1ff9bef5c37bee9ed2cb040644981faecc7197
639936 <hoahockibi@gmail.com>
2025-08-29 02:43:05 +0700

    up

--- Gemini-Baidu-translate-change-model/src/language_list.js
+++ Gemini-Baidu-translate-change-model/src/language_list.js
@@ -1,16 +1,15 @@
-
 let languages = [
     {"id": "zh", "name": "Trung"},
     {"id": "en", "name": "Anh"},
     {"id": "vi", "name": "Việt"},
-    {"id": "gen_gemini-2.5-pro", "name": "API (Gemini 2.5 Pro)"},
-    {"id": "gen_gemini-2.5-flash-preview-05-20", "name": "API (Flash Preview)"},
-    {"id": "gen_gemini-2.5-flash", "name": "API (Flash)"},
-    {"id": "gen_gemini-2.5-flash-lite", "name": "API (Flash Lite)"},
+    {"id": "gen_2.5pro", "name": "Model: 2.5 Pro"},
+    {"id": "gen_2.5flash_preview", "name": "Model: 2.5 Flash Preview"},
+    {"id": "gen_2.5flash", "name": "Model: 2.5 Flash"},
+    {"id": "gen_2.5flash_lite", "name": "Model: 2.5 Flash Lite"},
     {"id": "vi_tieuchuan", "name": "Việt (Tiêu chuẩn)"},
     {"id": "vi_NameEng", "name": "Việt (Name English)"},
     {"id": "vi_sac", "name": "Truyện Sắc"},
     {"id": "vi_vietlai", "name": "Viết Lại Convert"},
     {"id": "vi_layname", "name": "Lấy Name trong chương"},
-    {"id": "vi_xoacache", "name": "Xóa Cache Chương Này"} 
+    {"id": "vi_xoacache", "name": "Xóa Cache Chương Này"}
 ];
