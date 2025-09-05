95f6fc9e0631fe06ef4cd77c0378466f7da0e081
639936 <hoahockibi@gmail.com>
2025-09-05 19:03:02 +0700

    up

--- Local-book/src/config.js
+++ Local-book/src/config.js
@@ -1,1 +1,14 @@
-const BASE_URL = 'http://192.168.0.168:8080';
+let
+    HOST;
+const DEFAULT_HOST = 'http://192.168.0.168:8080';
+
+// 'local_ip' is a global variable injected by the app from the config
+if (typeof local_ip !== 'undefined' && local_ip) {
+    if (!local_ip.startsWith('http')) {
+        HOST = 'http://' + local_ip;
+    } else {
+        HOST = local_ip;
+    }
+} else {
+    HOST = DEFAULT_HOST;
+}
--- Local-book/src/detail.js
+++ Local-book/src/detail.js
@@ -1,22 +1,16 @@
 load('config.js');
+
 function execute(url) {
-        // --- Bắt đầu phần mã thêm vào ---
-    let current_host = localStorage.getItem('vp_local_ip');
-    if (!current_host) {
-        current_host = BASE_URL; // Dùng địa chỉ mặc định nếu chưa có trong storage
-    } else if (!current_host.startsWith('http')) {
-        current_host = 'http://' + current_host; // Tự động thêm http:// nếu người dùng quên
-    }
-    // --- Kết thúc phần mã thêm vào ---
     url = url.replace("/vBook/Book/", "");
     url = url.replace("http://localhost", "");
-    var response = fetch(current_host + "/vBook/Book/" + url);
+    var response = fetch(HOST + "/vBook/Book/" + url);
     if (response.ok) {
         var doc = response.html();
         return Response.success({
                 name: doc.select("#path").text().replace("/vBook/Book/", ""),
-                });
+        });
     }
-    else
-    return Response.success({name: url})
+    else {
+        return Response.success({name: url});
+    }
 }
