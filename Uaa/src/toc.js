a53603eef744eb0bdcfea497b53ffecb0e934899
639936 <hoahockibi@gmail.com>
2025-09-11 03:03:58 +0700

    up

--- Uaa/src/toc.js
+++ Uaa/src/toc.js
@@ -5,10 +5,11 @@ function execute(url) {
     if (response.ok) {
 
         let doc = response.html();
-        //console.log(json)
+        //console.log(doc)
         
-        let chapters=doc.select(".catalog_box .catalog_ul li.menu")
+        let chapters = doc.select(".catalog_box .catalog_ul li.menu, .catalog_box .catalog_ul li.child");
         chapters.forEach(e => {
+        //console.log(e)
         let k = e.select("span").text();
         if (k!=="游客" && k!=="注册会员") {k=""} else {k=">"}
         //.replace(/游客/g, ">").replace(/注册会员/g, ">")
